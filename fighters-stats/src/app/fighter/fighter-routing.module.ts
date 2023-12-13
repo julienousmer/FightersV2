5c135d0767eecb85cb582d67f4a26970ed4e6f7f
Borloo <supermaxime753@gmail.com>
2023-12-13 16:07:51 +0100

    admin component

--- fighters-stats/src/app/fighter/fighter-routing.module.ts
+++ fighters-stats/src/app/fighter/fighter-routing.module.ts
@@ -2,21 +2,23 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DetailFighterComponent} from "./detail-fighter/detail-fighter.component";
 import {FightersComponent} from "./fighters/fighters.component";
+import {AuthGuard} from "../utils/auth/auth.guard";
 
 const routes: Routes = [
   {
     path: 'fighter',
     children: [
-      {path: 'edit/:id', component: DetailFighterComponent, pathMatch: 'full'},
+      {path: 'edit/:id', component: DetailFighterComponent, pathMatch: 'full', canActivate:[AuthGuard]},
       {path: 'detail/:id', component: DetailFighterComponent, pathMatch: 'full'},
       {path: 'list', component: FightersComponent, pathMatch: 'full'},
       {redirectTo: 'list', path: '', pathMatch: 'full'}
     ]
   }
 ];
-
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 export class FighterRoutingModule {
---
+++ fighters-stats/src/app/models/Admin.ts
@@ -0,0 +1,5 @@
+export interface Admin {
+  id: number,
+  pseudo: string,
+  password: string
+}
