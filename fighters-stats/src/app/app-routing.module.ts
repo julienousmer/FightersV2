e3361644916f183c9399c26c3fa76133f297df9c
JulienOusmer <julienousmer46090@gmail.com>
2023-12-13 11:20:54 +0100

    Add configs routes

--- fighters-stats/src/app/app-routing.module.ts
+++ fighters-stats/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FightersComponent} from "./fighters/fighters.component";
+import {FighterComponent} from "./fighter/fighter.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'fighter/edit/:id', component: FighterComponent},
+  {path: 'fighter/list', component: FightersComponent},
+  {path: '', redirectTo: '/fighter/list', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
