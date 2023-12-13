7aa0759c8356bf80acce5ffa3e81b26e29d522e8
JulienOusmer <julienousmer46090@gmail.com>
2023-12-13 14:12:35 +0100

    load async modules in app-routing

--- fighters-stats/src/app/app-routing.module.ts
+++ fighters-stats/src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'fighter',
+    loadChildren: () => import('./fighter/fighter.module').then(m => m.FighterModule),
+  },
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
 ];
 
 @NgModule({
