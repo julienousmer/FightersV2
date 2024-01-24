b859a2ec77a2389c56bc9c2619ad6507b6581474
JulienOusmer <julienousmer46090@gmail.com>
2024-01-24 11:53:43 +0100

    Finish Entity Fighter backend

--- backend/src/fighters/entities/fighter.entity.ts
+++ backend/src/fighters/entities/fighter.entity.ts
@@ -1,18 +1,40 @@
 import {IFighter} from '@models/shared';
-import {Entity} from "typeorm";
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Category} from "../../categories/entities/category.entity";
 
 @Entity()
 export class Fighter implements IFighter {
 
-    age: number;
-    firstname: string;
-    height: number;
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({name: 'first_name', length: 50, nullable: false})
+    firstname: string;
+
+    @Column()
     lastname: string;
+
+    @Column({name: 'age', nullable: false})
+    age: number;
+
+    @Column({name: 'height', nullable: false})
+    height: number;
+
+    @Column({name: 'nb_lose', nullable: false})
     nbLose: number;
+
+    @Column({name: 'nb_win', nullable: false})
     nbWin: number;
+
+    @Column({name: 'reach', nullable: false})
     reach: number;
+
+    @Column({name: 'sexe', nullable: false})
     sexe: string;
+
+    @Column({name: 'weight', nullable: false})
     weight: number;
+    
+    @ManyToOne(() => Category, category => category.fighters)
     category: Category | null;
 }
