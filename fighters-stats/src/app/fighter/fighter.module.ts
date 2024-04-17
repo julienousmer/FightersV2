4f9177a2d41aee531d97956e625eafa186cf10aa
Borloo <supermaxime753@gmail.com>
2024-04-17 13:43:34 +0200

    Delete useless model + category component not used

--- fighters-stats/src/app/fighter/fighter.module.ts
+++ fighters-stats/src/app/fighter/fighter.module.ts
@@ -2,26 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FightersComponent} from "./fighters/fighters.component";
 import {DetailFighterComponent} from "./detail-fighter/detail-fighter.component";
-import {CategoryComponent} from "./category/category.component";
 import {EditFighterComponent} from "./edit/edit-fighter.component";
-import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
 import {FighterRoutingModule} from "./fighter-routing.module";
-
-
 
 @NgModule({
   declarations: [
     FightersComponent,
     DetailFighterComponent,
-    CategoryComponent,
     EditFighterComponent
   ],
   exports: [
     FightersComponent,
     DetailFighterComponent,
-    CategoryComponent,
     EditFighterComponent
   ],
   imports: [
@@ -32,3 +26,4 @@ import {FighterRoutingModule} from "./fighter-routing.module";
   ]
 })
 export class FighterModule { }
+
